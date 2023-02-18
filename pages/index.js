1c29784db246d60d9b3f15e3b7469326d4c62556
Evgenii Grigorev <eagrigorev87@gmail.com>
2023-02-18 22:03:23 +0200

    Raw [pageSlug] implementation

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Evgenii Grigorev • Home</title>
+      </Head>
+      <h1>Initial setup</h1>
+    </>
+  );
+};
+
+export default Home;
