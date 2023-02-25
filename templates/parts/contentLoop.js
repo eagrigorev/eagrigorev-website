111b0f6340fe1785f1013b087c8e7681e9b0ca8b
Evgenii Grigorev <eagrigorev87@gmail.com>
2023-02-25 14:57:39 +0200

    minor structure tweaks

--- templates/parts/contentLoop.js
+++ templates/parts/contentLoop.js
@@ -4,13 +4,15 @@ import IllustrationCard from './illustrationCard';
 const ContentLoop = ({ posts }) => {
   return (
     <>
-      {posts.map((post, index) => {
-        if (post.meta.contentType === 'post') {
-          return <PostCard key={index} post={post} />;
-        } else if (post.meta.contentType === 'illustration') {
-          return <IllustrationCard key={index} post={post} />;
-        }
-      })}
+      <section>
+        {posts.map((post, index) => {
+          if (post.meta.contentType === 'post') {
+            return <PostCard key={index} post={post} />;
+          } else if (post.meta.contentType === 'illustration') {
+            return <IllustrationCard key={index} post={post} />;
+          }
+        })}
+      </section>
     </>
   );
 };
--- templates/parts/header.js
+++ templates/parts/header.js
@@ -3,23 +3,25 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <>
-      <div>
+      <header>
         <div>
-          <h1>Evgenii Grigorev</h1>
+          <div>
+            <h1>Evgenii Grigorev</h1>
+          </div>
+          <div>Social links</div>
+          <div>Search</div>
         </div>
-        <div>Social links</div>
-        <div>Search</div>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 };
