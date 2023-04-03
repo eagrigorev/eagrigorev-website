f5d69e39b8c120285d527b1629aff98b5ec94d2e
Evgenii Grigorev <eagrigorev87@gmail.com>
2023-04-03 20:13:24 +0300

    Hello, World!

--- templates/post.js
+++ templates/post.js
@@ -3,7 +3,7 @@ import { marked } from 'marked';
 const Post = ({ props }) => {
   return (
     <>
-      <h2>{props.meta.title}</h2>
+      <h1>{props.meta.title}</h1>
       <p>Posted on {props.meta.datePosted}</p>
       <div dangerouslySetInnerHTML={{ __html: marked(props.content) }}></div>
       <div>Tags: {props.meta.tags}</div>
