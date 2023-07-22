c5e7ccf9337b2d3b04cd00f20ea117d5d1eb2213
Evgenii Grigorev <eagrigorev87@gmail.com>
2023-07-22 22:15:25 +0300

    Published 'Hello, World!"

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,18 +1,20 @@
+import { redirect } from 'next/navigation';
 import { getPostContent } from '../../utils/getPostContent';
 import Markdown from 'markdown-to-jsx';
 
 const Post = (props) => {
   const slug = props.params.slug;
-  const postContent = getPostContent().filter(
-    (post) => post.meta.slug === slug
-  );
-  return (
-    <>
-      {postContent.map((post) => (
+  const post = getPostContent().find((post) => post.meta.slug === slug);
+  if (post) {
+    return (
+      <>
+        <h2>{post.meta.title}</h2>
         <Markdown key={post.meta.slug}>{post.content}</Markdown>
-      ))}
-    </>
-  );
+      </>
+    );
+  } else {
+    redirect('/');
+  }
 };
 
 export default Post;
--- utils/relativeDate.tsx
+++ utils/relativeDate.tsx
@@ -1,5 +1,5 @@
 import { intlFormatDistance } from 'date-fns';
 
 export const relativeDate = (date: number | Date): string => {
-  return intlFormatDistance(date, new Date());
+  return intlFormatDistance(date, new Date(), { locale: 'en' });
 };
