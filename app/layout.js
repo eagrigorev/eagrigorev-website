e64253a4334136d7c44fcdc93987697548e71766
Evgenii Grigorev <eagrigorev87@gmail.com>
2023-12-28 20:41:56 +0200

    Basic setup

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,17 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from '@/components/header';
 
 export const metadata = {
   title: 'Evgenii Grigorev',
   description: 'Software developer & artist',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ components/header.js
@@ -0,0 +1,7 @@
+import style from './styling/header.module.scss';
+
+const Header = () => {
+  return <div className={style.header}>Header template for testing</div>;
+};
+
+export default Header;
