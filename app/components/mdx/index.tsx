8b750f589bd67fb2d314d2a9b37474fcb3699681
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-01-11 22:05:46 +0200

    Preparing the new setup

--- app/components/mdx/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css';
-
-export const SpacerParagraph = () => {
-  return <div className={styles.spacerParagraph}></div>;
-};
-
-export const SpacerHeading = () => {
-  return <div className={styles.spacerHeading}></div>;
-};
