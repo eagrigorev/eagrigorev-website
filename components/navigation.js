e9a3db3b85e4d04f3f9db181bbdf2678de09d6eb
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-02-11 14:16:05 +0200

    Responsive navigation

--- components/navigation.js
+++ components/navigation.js
@@ -1,19 +1,36 @@
 import styles from './navigation.module.scss';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navigation = () => {
   return (
     <nav className={styles['wrapper']}>
       <Link className={styles['left']} href={'/'}>
-        <p>Image</p>
-        <p>Evgenii Grigorev</p>
+        <Image className={styles['logo']} alt="Website Logo" />
+        <p className={styles['title']}>Evgenii Grigorev</p>
       </Link>
-      <ul className={styles['right']}>
+      <ul className={styles['right-desktop']}>
         <li>
-          <Link href={'/'}>Work</Link>
+          <Link className={styles['link']} href={'/'}>
+            Work
+          </Link>
         </li>
         <li>
-          <Link href={'/'}>Work</Link>
+          <Link className={styles['link']} href={'/'}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className={styles['link']} href={'/'}>
+            Now
+          </Link>
+        </li>
+      </ul>
+      <ul className={styles['right-mobile']}>
+        <li>
+          <Link className={styles['link']} href={'/'}>
+            Menu
+          </Link>
         </li>
       </ul>
     </nav>
