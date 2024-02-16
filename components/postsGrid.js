9c1d07f5c5fb89b0bd13a06341ac0434953b7695
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-02-16 23:15:14 +0200

    PostsGrid draft implementation

---
+++ components/postsGrid.js
@@ -0,0 +1,8 @@
+import styles from './postsGrid.module.scss';
+import PostCard from './postCard';
+
+const PostsGrid = () => {
+  return <div className={styles['wrapper']}></div>;
+};
+
+export default PostsGrid;
