3ffdb149b7388a5e89b66654d8792692761dfe26
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-03-03 14:35:07 +0200

    Dropdown menu logic

--- components/navigation.js
+++ components/navigation.js
@@ -3,8 +3,17 @@
 import styles from './navigation.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const Navigation = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showDropdownMobile, setShowDropdownMobile] = useState(false);
+  const handleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+  const handleDropdownMobile = () => {
+    setShowDropdownMobile(!showDropdownMobile);
+  };
   return (
     <nav className={styles['wrapper']}>
       <Link className={styles['left']} href={'/'}>
@@ -23,15 +32,35 @@ const Navigation = () => {
             Home
           </Link>
         </li>
-        <li className="link-dropdown" onClick={() => console.log('CLICK')}>
+        <li className="link-dropdown" onClick={handleDropdown}>
           <span className="link-underline">Categories</span>
         </li>
       </ul>
       <ul className={styles['right-mobile']}>
-        <li className="link-dropdown" onClick={() => console.log('CLICK')}>
+        <li className="link-dropdown" onClick={handleDropdownMobile}>
           <span className="link-underline">Menu</span>
         </li>
       </ul>
+      {showDropdown ? (
+        <ul className={`${styles['dropdown']} ${styles['right-desktop']}`}>
+          <li>
+            <Link className="link-color" href={'/music'}>
+              Music
+            </Link>
+          </li>
+        </ul>
+      ) : null}
+      {showDropdownMobile ? (
+        <ul
+          className={`${styles['dropdown-mobile']} ${styles['right-mobile']}`}
+        >
+          <li>
+            <Link className="link-color" href={'/music'}>
+              Music
+            </Link>
+          </li>
+        </ul>
+      ) : null}
     </nav>
   );
 };
