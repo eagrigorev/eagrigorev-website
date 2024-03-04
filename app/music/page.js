e6ce7917841bf151c23ec4237ccb00ded0210abc
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-03-04 16:47:44 +0200

    Music page fixes

--- app/music/page.js
+++ app/music/page.js
@@ -7,10 +7,9 @@ export default function Music() {
     <main className="container">
       <PageTitle
         title="Music"
-        subtitle="Boreal Heights is my musical project since 2013."
+        subtitle="Boreal Heights is my musical project since 2013. I started with melodic doom metal but later switched to dark folk genre."
       />
-      <section className="content-wrapper content-flex">
-        <Filters />
+      <section className="content-wrapper">
         <PostsGrid category="music" />
       </section>
     </main>
