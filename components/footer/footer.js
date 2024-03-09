56e7c01456064bdee8efa9a9a095305d469ecce1
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-03-09 19:47:21 +0200

    Footer fix

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles['wrapper']}>
       <div className="container grid">
-        <div className={styles['widgets--left']}>
+        <div className={`${styles['widgets']} ${styles['widgets--left']}`}>
           <h3 className={styles['title--left']}>
             Thank you for visiting! Want to stay updated?
           </h3>
@@ -26,21 +26,59 @@ const Footer = () => {
               </Link>
             </li>
           </ul>
+          <Link className={`link-color ${styles['privacy']}`} href={'/'}>
+            Privacy & Security
+          </Link>
         </div>
-        <div className={styles['widgets--right']}>
+        <div className={styles['widgets']}>
           <h4 className={styles['title--right']}>Links</h4>
           <div className={styles['links-wrapper']}>
             <ul className={styles['links']}>
               <li>
-                <Link className="link-underline" href={'/music'}>
-                  Music
+                <Link className="link-color" href={'/'}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link className="link-color" href={'/'}>
+                  Now
+                </Link>
+              </li>
+              <li>
+                <Link className="link-color" href={'/'}>
+                  Shop
+                </Link>
+              </li>
+              <li>
+                <Link className="link-color" href={'/'}>
+                  Colophon
+                </Link>
+              </li>
+              <li>
+                <Link className="link-color" href={'/'}>
+                  Library
                 </Link>
               </li>
             </ul>
             <ul className={styles['links']}>
               <li>
-                <Link className="link-underline" href={'/'}>
-                  Home
+                <Link className="link-color" href={'/'}>
+                  Illustrations
+                </Link>
+              </li>
+              <li>
+                <Link className="link-color" href={'/music-and-tabs'}>
+                  Music & Tabs
+                </Link>
+              </li>
+              <li>
+                <Link className="link-color" href={'/'}>
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link className="link-color" href={'/'}>
+                  Journal
                 </Link>
               </li>
             </ul>
