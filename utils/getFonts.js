7fb0a26b37b879251fba00a8a46f64e616919ace
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-05-15 14:27:26 +0300

    Adding new typography

--- utils/getFonts.js
+++ utils/getFonts.js
@@ -1,4 +1,4 @@
-import { Roboto, Roboto_Condensed } from 'next/font/google';
+import { Jost, Roboto, Roboto_Condensed, Spectral } from 'next/font/google';
 
 export const roboto = Roboto({
   weight: ['300', '400', '500', '700'], // Light, Regular, Medium, Bold
@@ -14,4 +14,20 @@ export const roboto_condensed = Roboto_Condensed({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-roboto-condensed',
+});
+
+export const jost = Jost({
+  weight: ['300', '400', '500'], // Light, Regular, Medium
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-jost',
+});
+
+export const spectral = Spectral({
+  weight: ['300'], // Light
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-spectral',
 });
