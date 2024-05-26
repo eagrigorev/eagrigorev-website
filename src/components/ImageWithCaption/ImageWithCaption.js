99cce40543664f466743e980b35d6b7f339cbe3d
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-05-26 23:46:42 +0300

    New images

--- src/components/ImageWithCaption/ImageWithCaption.js
+++ src/components/ImageWithCaption/ImageWithCaption.js
@@ -2,12 +2,13 @@ import styles from './ImageWithCaption.module.scss';
 import Image from 'next/image';
 
 const ImageWithCaption = ({ src, alt, caption, layout }) => {
+  const imageLayout = `layout--${layout}`;
   return (
-    <div className={styles[layout]}>
+    <div className={styles[imageLayout]}>
       <Image
         className={styles['image']}
         alt={alt}
-        src={`/images/${src}`}
+        src={`/images/featured/${src}`}
         width={920}
         height={920}
       />
