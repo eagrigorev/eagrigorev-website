e4bbed2c07ae77ade20c67f34260a6977813f57a
Evgenii Grigorev <eagrigorev87@gmail.com>
2024-06-12 21:12:37 +0300

    Fix prod deployment

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -13,10 +13,6 @@ const Page = (props) => {
   const allPosts = getAllPosts();
   const post = allPosts.find((post) => post.meta.slug === slug);
   if (categoriesList.includes(slug)) {
-    console.log(
-      'LOG: categoriesList.includes(slug)',
-      categoriesList.includes(slug)
-    );
     const postTypeByCategory = allPosts.find((post) => {
       return normalize(post.meta.category) === slug;
     });
